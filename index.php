21ff1473d5792900e4e26e35253b35e08183a378
mohamed <mehdaoui200309@gmail.com>
2024-03-07 14:49:59 +0100

    ertyulm

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// $_navar='hello';
+// echo "fuck the world <br> ";
+// $x=true;
+// $y=false;
+// // echo $x;
+// function test ($x){
+//     if($x){
+//         echo 'True';
+//     }
+//     else echo 'False';
+// }
+// // test($y);
+//     $x=true;
+//     $y=false;
+//     echo "true :". $x."false:". $y .  "<br>"; 
+//     print "true :". $x."false:". $y .  "<br>"; 
+//     $a=15;
+//     echo($a%3== 0&& $a%5==0?"true":"false");
+//     echo "<br>"; 
+//     $age=30;
+//     $sexe="f";
+//     echo($age>21 && $age<40 && $sexe=='f'?"bonjour vous êtes".$age:"false");
+$i=0;
+// do{
+//     $random1 =rand(0,10);
+//     $random2 =rand(0,10);
+//     $random3 =rand(0,10);
+//     $i++;
+// }while(!($random1%2 == 0 && $random2%2!=0 &&  $random3%2!=0));
+// echo $random1. " ".$random2. " ".$random3 ."<br>";
+// echo $i;
+
+$num=rand(111,999);
+while($num!=333){
+    $i++;
+    $num=rand(0,999);
+}
+echo $i;
+
